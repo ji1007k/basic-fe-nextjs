033db470af71e56688bd15a52b1e4070bce7f9e7
jungin <wjddls141093@duksung.ac.kr>
2025-04-24 17:07:29 +0900

    1. chore: Swagger 요청 프록시 분기 처리 및 로컬 개발용 HTTP 서버 설정 최신화 - Swagger 페이지 요청과 기타 API 요청에 대해 프록시 경로 분기 처리 로직 추가 - 로컬 HTTP 서버 설정을 최신화하여 개발 환경 개선

--- src/server/httpServer.js
+++ src/server/httpServer.js
@@ -4,11 +4,23 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import express from "express";
 import * as https from "node:https";
 import dotenv from 'dotenv';
+import nextConfig from "../../next.config.mjs";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-// .env.local 파일을 로드 (EXPRESS)
+// 1. 항상  .env.local 파일을 로드 (EXPRESS)
 dotenv.config({ path: '.env.local' });
+
+// 2. 환경별 `.env` 파일 추가 로드
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+dotenv.config({ path: envFile });
+
+const useRemoteAPI = process.env.USE_REMOTE_API == 'true';
+const API_URL = useRemoteAPI ? process.env.API_URL_PROD : process.env.API_URL_LOCAL;
+const WS_URL = useRemoteAPI ? process.env.WS_URL_PROD : process.env.WS_URL_LOCAL;
+
+console.log("Mode: ", process.env.NODE_ENV);
+console.log(`Loaded .env.local and ${envFile}`);
+console.log("API Server: ", API_URL);
+console.log("WebSocket URL: ", WS_URL);
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -35,36 +47,82 @@ server.use("/api", (req, res, next) => {
 
 // 🔥 **프록시 설정** (배포환경에서 비활성화)
 const proxyOptions = {
-    target: process.env.NEXT_PUBLIC_API_URL, // API 서버
+    target: API_URL, // API 서버
     changeOrigin: true,  // 프록시 요청의 Origin 헤더를 타겟 서버의 도메인으로 바꿈
-    crossOrigin: true,
-    pathRewrite: { "^/api": "" },   // local 서버 요청 시 api 경로 제거
     logLevel: 'debug',  // 로그 레벨을 설정하여 프록시 로그 확인 가능,
-    secure: false, // SSL 인증서 검증 비활성화 (로컬 개발용)
-    agent: new https.Agent({ rejectUnauthorized: false }), // 자체 서명 SSL 허용
+};
+
+if (API_URL.startsWith("https")) {
+    Object.assign(proxyOptions, {
+        pathRewrite: (path, req) => {
+            return req.originalUrl; // req.originalUrl는 "/api/auth/login"을 포함함.
+        },
+        secure: false,  // SSL 인증서 검증 비활성화 (로컬 개발용)
+        agent: new https.Agent({ rejectUnauthorized: false }),  // 자체 서명 SSL 허용
+    });
+} else {
+    Object.assign(proxyOptions, {
+        pathRewrite: (path, req) => {
+            // swagger 관련 요청에선 그대로 사용
+            if (["swagger", "/v3/api-docs"].some(keyword => path.includes(keyword))) {
+                return req.originalUrl;
+            }
+            // 그 외 url에서 api 제거
+            return path.replace(/^\/api/, "");
+        }
+    });
 }
 
-console.log(process.env.NODE_ENV, dev);
+server.use(
+    "/api",
+    createProxyMiddleware(proxyOptions)
+);
 
-if (dev) {
-    server.use(
-        "/api",
-        createProxyMiddleware(proxyOptions)
-    );
+
+// ==========================================
+// WebSocket 프록시 설정
+const wsProxyOptions = {
+    target: WS_URL,         // 실제 WebSocket 서버 주소
+    pathFilter: '/ws/',     // 프록시할 경로
+    ws: true,               // WebSocket 연결을 처리
 }
 
+if (useRemoteAPI) {
+    Object.assign(wsProxyOptions, {
+        secure: false,                              // SSL 인증서 검증 비활성화 (로컬 개발용)
+        pathRewrite: (path, req) => req.originalUrl  // 원래 경로 그대로 사용 (배포 서버)
+    });
+} else {
+    Object.assign(wsProxyOptions, {
+        pathRewrite: { "^/ws/": "/chat" },           // 로컬 개발 시 '/ws/'를 '/chat'으로 변경
+    });
+}
+
+const wsProxyMiddleware = createProxyMiddleware(wsProxyOptions);
+
+// HTTP 'upgrade' 요청을 처리하여 WebSocket 연결을 허용
+server.on('upgrade', wsProxyMiddleware.upgrade);
+
+// WebSocket 요청을 처리하는 미들웨어 설정
+server.use(wsProxyMiddleware);
+
+
+// ==========================================
 // Next.js의 기본 라우팅을 처리
 server.all("*", (req, res) => {
     return handle(req, res);    // Next.js에서 클라이언트 요청을 처리하는 기본 함수
 });
 
-const port = 3000;
-
 // HTTPS 서버 실행
+const PORT = process.env.PORT || 3000;
+
+// 💡 basePath 접근
+const basePath = nextConfig.basePath || '/';
+
 app.prepare().then(() => {
-    createServer(server).listen(port, (err) => {
+    createServer(server).listen(PORT, (err) => {
         if (err) throw err;
-        console.log("🚀 Server running at http://localhost:" + port);
+        console.log(`🚀 Server running at http://localhost:${PORT}${basePath}`);
     });
 });
 
